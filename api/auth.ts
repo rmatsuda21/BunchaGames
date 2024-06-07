e1b6e267f4b6ee83036f6ba0e7aba6d966efe818
rmatsuda21 <reo.matsuda@gmail.com>
2024-06-07 13:38:42 -0700

    fix auth

--- api/auth.ts
+++ api/auth.ts
@@ -12,7 +12,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     client_secret: process.env.START_GG_OAUTH_SECRET,
     grant_type: "authorization_code",
     code: code,
-    redirect_uri: "https://buncha-games.vercel.app",
+    redirect_uri: "https://buncha-games.vercel.app/api/auth",
     scope: "user.identity",
   };
 
@@ -31,5 +31,5 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   const json = await _res.json();
 
   res.setHeader("set-cookie", `test=abc; HttpOnly;`);
-  return res.status(200).json({ code, json });
+  return res.status(200).json({ code, json, data });
 }
