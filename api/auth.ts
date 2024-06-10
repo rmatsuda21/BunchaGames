5e4c3ee4e53c01bb3227a04c359137658a9694be
rmatsuda21 <reo.matsuda@gmail.com>
2024-06-10 14:36:20 -0700

    bring back auth

--- api/auth.ts
+++ api/auth.ts
@@ -4,61 +4,45 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   const code = (req.query?.code as string) || "";
 
   if (!code) {
-    res.setHeader(
-      "Set-Cookie",
-      `start-gg-auth='34953409'; Domain=${req.headers.host}; Secure; HttpOnly;`
-    );
     return res.status(400).json("No code provided!");
   }
 
-  // const data = {
-  //   client_id: process.env.CLIENT_ID,
-  //   client_secret: process.env.START_GG_OAUTH_SECRET,
-  //   grant_type: "authorization_code",
-  //   code: code,
-  //   redirect_uri: "https://buncha-games.vercel.app/api/auth",
-  //   scope: "user.identity",
-  // };
+  const data = {
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.START_GG_OAUTH_SECRET,
+    grant_type: "authorization_code",
+    code: code,
+    redirect_uri: "https://buncha-games.vercel.app/api/auth",
+    scope: "user.identity",
+  };
 
-  // const headers = {
-  //   "Content-Type": "application/json",
-  // };
+  const headers = {
+    "Content-Type": "application/json",
+  };
 
-  // const _res = await fetch("https://api.start.gg/oauth/access_token", {
-  //   method: "POST",
-  //   headers,
-  //   body: JSON.stringify(data),
-  // });
+  const _res = await fetch("https://api.start.gg/oauth/access_token", {
+    method: "POST",
+    headers,
+    body: JSON.stringify(data),
+  });
 
-  // const json = await _res.json();
+  const json = await _res.json();
 
-  // const { access_token, expires_in, refresh_token } = json;
-  // const expiration_date = new Date(
-  //   Date.now() + expires_in * 1000
-  // ).toISOString();
+  const { access_token, expires_in, refresh_token } = json;
+  const expiration_date = new Date(
+    Date.now() + expires_in * 1000
+  ).toISOString();
 
-  // res.setHeader(
-  //   "Set-Cookie",
-  //   `start-gg-auth=${JSON.stringify({
-  //     access_token,
-  //     expiration_date,
-  //     refresh_token,
-  //   })}; HttpOnly; SameSite=Strict; Expires=${expires_in}}; Domain=${
-  //     req.headers.host
-  //   };`
-  // );
+  const cookie = `start-gg-auth=${JSON.stringify({
+    access_token,
+    expiration_date,
+    refresh_token,
+  })}`;
+
   res.setHeader(
-    "set-cookie",
-    `start-gg-auth='34598'; Path=/; Secure; HttpOnly; SameSite=Strict;`
+    "Set-Cookie",
+    `${cookie} HttpOnly; Path=/; Secure; HttpOnly; SameSite=Strict;`
   );
-  // window.localStorage.setItem(
-  //   "start-gg-auth",
-  //   JSON.stringify({
-  //     access_token,
-  //     expiration_date,
-  //     refresh_token,
-  //   })
-  // );
 
   return res.redirect(308, `/`);
 }
