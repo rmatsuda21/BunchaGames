105feb57ce1020346c6c5bff067b827175fc4bb0
rmatsuda21 <reo.matsuda@gmail.com>
2024-12-23 18:37:35 -0500

    improve cookie

--- src/pages/startgg/api.tsx
+++ src/pages/startgg/api.tsx
@@ -1,24 +1,36 @@
 import Cookies from "js-cookie";
 import { useEffect } from "react";
 
+const replaceAndRedirect = () => {
+  window.history.replaceState({}, "", "/");
+  window.location.href = "/";
+};
+
 const RediectPage = () => {
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
+
+    if (!params.has("startggtoken")) {
+      replaceAndRedirect();
+      return;
+    }
+
     const token = JSON.parse(String(params.get("startggtoken")));
 
-    const value = {
-      access_token: token.access_token,
-      refresh_token: token.refresh_token,
-    };
+    if (!token.access_token || !token.expiration_date || !token.refresh_token) {
+      replaceAndRedirect();
+      return;
+    }
 
     const expiration = new Date(token.expiration_date);
-
-    Cookies.set("start-gg-auth", JSON.stringify(value), {
+    Cookies.set("start-gg-token", token.access_token, {
       expires: expiration,
     });
+    Cookies.set("start-gg-refresh", token.refresh_token, {
+      expires: new Date(expiration.getDate() + 7),
+    });
 
-    window.history.replaceState({}, document.title, "/");
-    window.location.href = "/";
+    replaceAndRedirect();
   }, []);
 
   return null;
